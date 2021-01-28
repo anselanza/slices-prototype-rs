f050d921bf19dfda4ace55944f084f2ba5c8d828
Stephen Buchanan <stephen.buchanan@random.studio>
2021-01-28 15:27:13 +0100

    width changes directly ok

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,13 @@ struct Box {
     id: u8,
     width: f32,
     target_width: f32,
-    left: f32,
-    target_left: f32,
+    centre: f32,
+    target_centre: f32,
+}
+
+impl Box {
+    fn left (&self) -> f32 { self.centre - self.width / 2.0 }
+    fn right (&self) -> f32 { self.centre + self.width / 2.0 }
 }
 
 fn main() {
@@ -34,33 +39,33 @@ fn model(_app: &App) -> Model {
             id: i,
             width: standard_width,
             target_width: standard_width,
-            left: (i as f32) * standard_width - window_width/2.0 + standard_width/2.0,
-            target_left: (i as f32) * standard_width,
+            centre: (i as f32) * standard_width - window_width/2.0 + standard_width/2.0,
+            target_centre: (i as f32) * standard_width,
         }).collect(),
         box_size: 0.0
     }
 }
 
 fn is_in_box (x: f32, b: &Box) -> bool {
-    x > b.left && x < (b.left + b.width)
+    x > b.left() && x < b.right()
 }
 
-fn layout_boxes(boxes: &mut Vec<Box>, window_width: f32, max_width: f32, target_x: f32) {
-    let min_width = window_width / boxes.len() as f32;
+fn layout_boxes(boxes: &mut Vec<Box>, window_rect: Rect, target_size: f32, target_x: f32) {
+    let min_width = window_rect.w() / boxes.len() as f32;
     let active_box = boxes.into_iter().find(|b| is_in_box(target_x, b));
 
-    if target_x < 0.0 || target_x > window_width { // bail out if target out of window bounds
+    if target_x < window_rect.left() || target_x > window_rect.right() { // bail out if target out of window bounds
         return;
     }
 
     // active_box is an Option, so need to get the result if it exists
     match active_box {
         Some(b) => {
-            let in_x = if max_width > min_width { target_x - max_width / 2.0 } else { b.left };
-            // b.target_left = clamp(in_x, 0.0, window_width);
+            let in_x = if target_size > min_width { target_x - target_size / 2.0 } else { b.centre };
+            // b.target_centre = clamp(in_x, 0.0, window_width);
             // b.target_width = clamp_max(max_width, min_width);
             // b.left = clamp(in_x, -window_width/2.0, window_width/2.0);
-            // b.width = clamp_max(max_width, min_width);
+            b.width = clamp(target_size, min_width, target_size);
         },
         _ => {}
     }
@@ -74,7 +79,7 @@ fn update(_app: &App, _model: &mut Model, _update: Update) {
     let box_size: f32 = map_range(_app.mouse.position().y, window_rect.top(), window_rect.bottom(), window_rect.w(), 0.0);
     _model.box_size = box_size;
 
-    layout_boxes(&mut _model.boxes, window_rect.w(), box_size, _app.mouse.position().x);
+    layout_boxes(&mut _model.boxes, window_rect, box_size, _app.mouse.position().x);
 
 }
 fn view(_app: &App, _model: &Model, frame: Frame){
@@ -90,7 +95,7 @@ fn view(_app: &App, _model: &Model, frame: Frame){
             .color(if is_in_box(_app.mouse.position().x, b) { GREEN } else { GREY })
             .w(b.width)
             .h(window_rect.h())
-            .x(b.left)
+            .x(b.centre)
             .y(0.0)
             .stroke_weight(2.0)
             .stroke(BLUE);
