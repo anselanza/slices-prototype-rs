071705af881b22a1bcf62beb6875ab5e5599a4aa
Stephen Buchanan <stephen.buchanan@random.studio>
2021-01-29 11:21:29 +0100

    generic way to squeeze boxes

--- src/main.rs
+++ src/main.rs
@@ -56,6 +56,17 @@ fn is_in_box (x: f32, b: &Box) -> bool {
     x > b.left() && x < b.right()
 }
 
+fn squeeze_box_to_fit (b: &Box, start_x: f32, offset_index: i8, count: u8, space_remaining: f32) -> Box {
+    let new_width = space_remaining / count as f32;
+    Box {
+        id: b.id,
+        width: b.width,
+        target_width: new_width,
+        centre: b.centre,
+        target_centre: start_x + new_width/2.0 + (offset_index + b.id as i8) as f32 * new_width,
+    }
+}
+
 fn layout_boxes(boxes: &Vec<Box>, active_box: &Box, window_rect: Rect, target_size: f32, target_x: f32) -> Vec<Box>{
     let min_width = window_rect.w() / boxes.len() as f32;
 
@@ -72,42 +83,15 @@ fn layout_boxes(boxes: &Vec<Box>, active_box: &Box, window_rect: Rect, target_si
             if b.id < active_box.id { // left
                 let num_boxes = active_box.id;
                 let left_space_remaining = abs(active_box.left() - window_rect.left());
-                let left_width = left_space_remaining / num_boxes as f32;
-                Box {
-                    id: b.id,
-                    width: b.width,
-                    target_width: left_width,
-                    centre: b.centre,
-                    target_centre: window_rect.left() + b.id as f32 * left_width + left_width/2.0,
-                }
+                return squeeze_box_to_fit(b, window_rect.left(), 0, num_boxes, left_space_remaining);
             } else { // right
-                let right_start = active_box.right();
-                let right_start_index = active_box.id + 1;
+                let right_start_index: i8 = active_box.id as i8 + 1;
                 let right_space_remaining = abs(window_rect.right() - active_box.right());
                 let num_boxes = boxes.len() as u8 -1 - active_box.id;
-                let right_width = right_space_remaining /   num_boxes as f32;
-                Box {
-                    id: b.id,
-                    width: b.width,
-                    target_width: right_width,
-                    centre: b.centre,
-                    target_centre: right_start + (b.id - right_start_index) as f32 * right_width + right_width/2.0,
-                }
+                return squeeze_box_to_fit(b, active_box.right(), -right_start_index, num_boxes, right_space_remaining);
             }
         }
     }).collect();
-
-    // let mut box_update = active_box.clone();
-    // box_update.target_centre = clamp(target_x, window_rect.left(), window_rect.right());
-    // box_update.target_width = clamp(target_size, min_width, target_size);
-
-    // let left_slices = &boxes[0.. active_box.id as usize];
-    // let start_right = active_box.id + 1;
-    // let right_slices = &boxes[start_right as usize.. boxes.len()];
-
-
-    // let a: &[Box] = &[box_update];
-    // let updated = [left_slices, a, right_slices].concat();
 
     return updated;
 
@@ -115,14 +99,13 @@ fn layout_boxes(boxes: &Vec<Box>, active_box: &Box, window_rect: Rect, target_si
 
 fn animate_boxes(boxes: &mut Vec<Box>) {
     for b in boxes {
+        const LERP_FACTOR: f32 = 0.1;
         {
             let range = Range { start: b.width, end: b.target_width };
-            const LERP_FACTOR: f32 = 0.1;
             b.width = range.lerp(LERP_FACTOR);
         }
         {
             let range = Range { start: b.centre, end: b.target_centre };
-            const LERP_FACTOR: f32 = 0.1;
             b.centre = range.lerp(LERP_FACTOR);
         }
     }
@@ -146,8 +129,8 @@ fn update(_app: &App, _model: &mut Model, _update: Update) {
         _ => {}
     }
     animate_boxes(&mut _model.boxes);
-
 }
+
 fn view(_app: &App, _model: &Model, frame: Frame){
     // frame.clear(PURPLE);
     let draw = _app.draw();
